865f701d4e077273d9c29fb0f9927940aec8613e
AdrianaAnca <44239605+AdrianaAnca@users.noreply.github.com>
2018-12-04 23:19:12 +0200

    Update tema1_ex3_functie.m

--- tema1_ex3_functie.m
+++ tema1_ex3_functie.m
@@ -1,6 +1,10 @@
 function[mediaarm,vector,matrice]=functie(z)
-mediaarm=mean(real(z))%calculam media aritmetrica a partilor reale
-vector=z.^2%se ridica la patrat elementele vectorului
-matrice=z'*z%se inmulteste vectorul cu transpusa lui
+mediaarm=0
+for i=1:1:3
+mediaarm=mediaarm+real(z(i))
+end
+mediaarm/3 %se afiseaza media aritmedica a partilor reale
+vector=z.^2 %se ridica la patrat elementele vectorului
+matrice=z'*z %se inmulteste vectorul cu transpusa lui
 
 end
