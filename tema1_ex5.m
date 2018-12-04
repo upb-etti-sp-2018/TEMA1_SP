41bb924830eba0435fedef0a9871187e50e9dc94
AdrianaAnca <44239605+AdrianaAnca@users.noreply.github.com>
2018-12-05 00:08:51 +0200

    Update tema1_ex5.m

--- tema1_ex5.m
+++ tema1_ex5.m
@@ -1,28 +1,25 @@
 %Exercitiul5
-%a) Modificati pasul de variaþie a variabilei t la 0.01 ºi apoi la 0.0002.
-%Comentaþi diferentele.
-%b) Masurati pe grafic perioada semnalului sinusoidal în cele 3 situaþii.
-%c) Generati un semnal cosinusoidal de frecvenþã 20 Hz pe care sã-l reprezentaþi
-%cu culoare roºie pe acelaºi grafic peste semnalul sinusoidal.
+%a) Modificati pasul de variaÅ£ie a variabilei t la 0.01 ÅŸi apoi la 0.0002.
+%ComentaÅ£i diferentele.
+%b) Masurati pe grafic perioada semnalului sinusoidal Ã®n cele 3 situaÅ£ii.
+%c) Generati un semnal cosinusoidal de frecvenÅ£Äƒ 20 Hz pe care sÄƒ-l reprezentaÅ£i
+%cu culoare roÅŸie pe acelaÅŸi grafic peste semnalul sinusoidal.
 
 
 
-F=50;
-t1=0:0.01:0.2;
+F=50
+t1=0:0.01:0.2
 s1=2*sin(2*pi*F*t1);
-figure(1)
 plot (t1,s1,'.-'),xlabel('Timp [s1]'), grid
 
 t2=0:0.0002:0.2;
 s2=2*sin(2*pi*F*t2);
-figure(2)
 plot (t2,s2,'.-'),xlabel('Timp [s2]'), grid
 
 t3=0:0.001:0.2;
 s3=2*sin(2*pi*F*t3);
-figure(3)
 plot (t3,s3,'.-'),xlabel('Timp [s3]'), grid
-
-hold on
-c3=2*cos(2*pi*F*t3);
-plot(t3,c3,'.-'),xlabel('Timp [s3]'), grid
+%In cele 3 situatii perioada semnalului este aceeasi si anume 0,2 secunde
+F1=20
+c3=2*cos(2*pi*F1*t3);
+plot(t3,c3,'r.-'),xlabel('Timp [s3]'), grid %litera r indica culoarea rosie
