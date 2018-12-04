f3c51f7f29ad42ffe70eeacd28c017557e1a0895
AdrianaAnca <44239605+AdrianaAnca@users.noreply.github.com>
2018-12-04 23:51:22 +0200

    Update tema1_ex4.m

--- tema1_ex4.m
+++ tema1_ex4.m
@@ -1,23 +1,23 @@
 z=[0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0]
 n=0:20
 m=-5:15
-
-figure(1)
+%a)
 subplot(2,1,1)
 stem(n,z)
 subplot(2,1,2)
 stem(m,z)
-figure(2)
-t=abs(10-n)
-plot(n,t)
+
+%b)
+t=abs(10-n) %t=|10-n|
+plot(n,t) %reprezinta grafic t in functie de n=0:20
+
+%c)
 n1=-15:25
 x1=sin((pi/17)*n1)
-figure(3)
-plot(n1,x1)
 n2=0:50
 x2=cos((pi/sqrt(23))*n2)
-hold on
-plot(n2,x2)
+plot(n1,x1,n2,x2)
+
 subplot(2,1,1)
 plot(n1,x1)
 subplot(2,1,2)
