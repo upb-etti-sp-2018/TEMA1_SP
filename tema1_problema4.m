9b4bd3cd214479a637cc5a28fb1e1722c7554340
AdrianaAnca <44239605+AdrianaAnca@users.noreply.github.com>
2018-12-12 00:43:24 +0200

    Update tema1_problema4.m

--- tema1_problema4.m
+++ tema1_problema4.m
@@ -1,14 +1,13 @@
-T=10;
-%T0 e perioada si f0 e frecventa
 T0=2;
 f0=1/T0;
-t1=0:0.2:T;
-t2=0:0.02:T;
-t3=0:0.002:T;
+t1=0:0.2:10;
+t2=0:0.02:10;
+t3=0:0.002:10;
 %inmultim cu 0.8 pt ca vrem sa avem amplitudine de 0.8
 s1=0.8*sin(2*pi*f0*t1);
 s2=0.8*sin(2*pi*f0*t2);
 s3=0.8*sin(2*pi*f0*t3);
+%componentele mai mici decat zero le facem nule
 for i=1:length(s1)
 if (s1(i)<0)
 s1(i)=0;
@@ -24,7 +23,6 @@ if (s3(i)<0)
 s3(i)=0;
 end;
 end;
-%se cauta componentele negative si se egaleaza cu 0
 subplot(3,1,1)
 plot(t1,s1)
 subplot(3,1,2)
